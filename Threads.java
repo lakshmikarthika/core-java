a1d5de223dca9317d303134ec5cc1140f1f04735
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2023-11-09 00:17:36 +0530

    Update Threads.java

--- Threads.java
+++ Threads.java
@@ -1,34 +1,42 @@
 class A extends Thread{
-    public void run(){
-        for(int i =1; i<=100; i++){
-            System.out.println("Hi");
-            try{
-                Thread.sleep(10);
-            }
-            catch(InterruptedException e){
-
-            }
-        }
-    }
+	public void run() {        // when thread is started JVM calls the run method
+		show();
+	} 
+	public void show() {
+		for(int i=0;i<50;i++) {
+			System.out.println("Hi");
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				
+			}
+		}
+		
+	}
 }
 class B extends Thread{
-    public void run(){
-        for(int i =1; i<=100; i++){
-            System.out.println("Hello");
-            try{
-                Thread.sleep(10);
-            }
-            catch(InterruptedException e){
+	public void run() {
+		show();
+	}
+	public void show() {
+		for(int i=0;i<50;i++) {
+			System.out.println("Hello");
+			try {
+				Thread.sleep(6);
+			} catch (InterruptedException e) {
                 
-            }
-        }
-    }
+			}
+		}
+	}
 }
-public class Threads{
-    public static void main(String[] args){
-        A obj1 = new A();
-        B obj2 = new B();
-        obj1.start();
-        obj2.start();
-    }
+public class ThreadPractise {
+	public static void main(String[] args) {
+		A obj = new A();
+		B obj1 = new B();
+		System.out.println(obj.getPriority());
+		obj.start();
+		obj1.start();
+		obj1.setPriority(Thread.MAX_PRIORITY);
+		System.out.println(obj1.getPriority());
+	}
 }
