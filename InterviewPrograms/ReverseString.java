066ebf90aa51e83a20e081f6b424d74a1b56e6dd
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2023-11-10 18:50:32 +0530

    Update ReverseString.java

--- InterviewPrograms/ReverseString.java
+++ InterviewPrograms/ReverseString.java
@@ -9,9 +9,20 @@ public class StringReverse {
 		String s2 =sb.toString();
 		System.out.println(s2);
 
-  // using reverse method from string builder
-    String str = "Morning";
+  		// using reverse method from string builder
+    		String str = "Morning";
 		StringBuilder sb1 = new StringBuilder(str);
 		System.out.println(sb1.reverse());
+
+
+		// using string concatenation
+		String name = "lucky";
+		char[] nameArr = name.toCharArray();
+		String rev = "";
+		for(int i = nameArr.length-1; i>=0; i--) {
+			rev += nameArr[i];
+			
+		}
+		System.out.println(rev);
 	}
 }
